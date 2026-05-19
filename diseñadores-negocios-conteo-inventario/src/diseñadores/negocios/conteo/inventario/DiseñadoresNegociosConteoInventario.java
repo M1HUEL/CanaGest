7821af9b775214c003d45404fa55e6f22e085389
M1HUEL <imiguelx008@gmail.com>
2026-05-18 20:23:48 -0700

    Agregar subsistema de conteo inventario con sus dependencias

---
+++ diseñadores-negocios-conteo-inventario/src/diseñadores/negocios/conteo/inventario/DiseñadoresNegociosConteoInventario.java
@@ -0,0 +1,9 @@
+package diseñadores.negocios.conteo.inventario;
+
+public class DiseñadoresNegociosConteoInventario {
+
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+
+}
